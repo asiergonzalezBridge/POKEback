e13bb2f26223b05941529ea677390f18f1324ab2
Eli <hidragyrum@gmail.com>
2026-04-21 15:41:52 +0200

    borrando idex router y package json

--- src/routers/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import teamRouter from "./teamRouter.js";
-
-const router = Router();
-
-// Aquí conectamos las rutas de equipo al prefijo /team
-router.use("/team", teamRouter);
-
-export default router;
